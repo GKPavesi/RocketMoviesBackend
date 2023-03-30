67ae55d7a35984e974d10e2948139dbc5bffb32d
GKPavesi <guipavesi@hotmail.com>
2023-03-29 23:48:27 -0300

    migrations created

--- src/database/knex/migrations/20230330021121_createUsers.js
+++ src/database/knex/migrations/20230330021121_createUsers.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-    
-};
+exports.up = knex => knex.schema.createTable("users", table => {
+    table.increments("id");
 
-exports.down = function(knex) {
-  
-};
+    table.text("name").notNullable();
+    table.text("email").notNullable();
+    table.text("password").notNullable();
+    table.text("avatar");
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+})
+
+exports.down = knex => knex.schema.dropTable("users");
